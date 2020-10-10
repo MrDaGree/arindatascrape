c5c445cc7a489ef6d5a839d3d0c333a3a62a9929
Dawson Greeley <dawsonmax10@gmail.com>
2020-10-10 14:12:57 -0500

    Added progress bar for regular search queries, added customer ID search

--- arin.py
+++ arin.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import argparse
+import sys
 import xml.etree.ElementTree as ET
 
 #setup runtime terminal arguments
@@ -13,61 +14,90 @@ args = parser.parse_args()
 if not args.search:
     exit()
 
+def progres(count, total, status='', bar_len=60):
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = str(count) + " / " + str(total)
+    bar = '█' * filled_len + '-' * (bar_len - filled_len)
+
+    fmt = '%s [%s] %s' % (status, bar, percents,)
+    print('\b' * len(fmt), end='')  # clears the line
+    sys.stdout.write(fmt)
+    sys.stdout.flush()
+
+def countCustomers(tablerows):
+    count = 0
+
+    for row in tablerows:
+        cells = row.find_all("td")
+        if cells:
+            #if the cell has a length of less than 2 is the customer link on main page.
+            if len(cells) < 2:
+                #get customer link from table cell
+                customer_link = cells[0].find('a').get('href')
+                #ensure the link found is a customer link, sometimes org links and those arent helpful in this case
+                if "https://whois.arin.net/rest/customer/" in customer_link:
+                    count += 1
+
+    if args.limit: 
+        if args.limit < count:
+            count = args.limit
+
+    return count
+
+def customerScrape(link):
+    customer_soup = BeautifulSoup(requests.get(link).text, 'xml')
+    netref = customer_soup.find('netRef')
+    addr = netref['startAddress'] + " - " + netref['endAddress'] + " | Customer ID: " + customer_soup.find('handle').text
+
+    return addr
+
 #list of known addresses after the scrape to then output for user
 known_ip_addresses = []
 
-#inital beautfulsoup query on search to get list of addition "customers" as well.
-request = {
-    'queryinput': args.search 
-}
-page = requests.post('https://whois.arin.net/ui/query.do', data=request)
-soup = BeautifulSoup(page.text, 'html.parser')
-querycontent = soup.find('div', {"id": "maincontent"})
-tablerows = querycontent.find('table').find_all("tr")
+if "C0" in args.search:
+    #append the IP address of the customer to the known IP addresses
+    known_ip_addresses.append(customerScrape("https://whois.arin.net/rest/customer/" + args.search))
+else:
+    #inital beautfulsoup query on search to get list of addition "customers" as well.
+    request = {
+        'queryinput': args.search 
+    }
+    page = requests.post('https://whois.arin.net/ui/query.do', data=request)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    querycontent = soup.find('div', {"id": "maincontent"})
+    tablerows = querycontent.find('table').find_all("tr")
 
-#customer link counter in case user only wants to do a certain few
-customer_link_check = 0
+    #customer link counter for progress bar
+    customer_link_check = 0
+    #total customers returned from 'countCustomers' function which either scrapes total customers off main page or returns the user limit
+    total_customers = countCustomers(tablerows)
 
-#loop through main page table
-for row in tablerows:
-    cells = row.find_all("td")
-    if cells:
-        #if the cell has a length of 2 it includes an IP address in the table on main page
-        if len(cells) == 2:
-            known_ip_addresses.append(cells[1].text)
-        #the cell length isnt 2 so it has to be a customer ID with link
-        else:
-            #get customer link from table cell
-            customer_link = cells[0].find('a').get('href')
-            #ensure the link found is a customer link, sometimes org links and those arent helpful in this case
-            if "https://whois.arin.net/rest/customer/" in customer_link:
-                customer_link_check += 1
-                print(str(customer_link_check) + " : " + customer_link)
-                #check if user only wanted to scrape a few customers or all
-                if args.limit:
-                    #check if we are at the user supplied customer check limit
-                    if (customer_link_check < args.limit):
-                        #parse the customer data as an XML
-                        customer_soup = BeautifulSoup(requests.get(customer_link).text, 'xml')
-                        netref = customer_soup.find('netRef')
-                        addr = netref['startAddress'] + " - " + netref['endAddress']
+    #loop through main page table
+    for row in tablerows:
+        cells = row.find_all("td")
+        if cells:
+            #if the cell has a length of 2 it includes an IP address in the table on main page
+            if len(cells) == 2:
+                known_ip_addresses.append(cells[1].text)
+            #the cell length isnt 2 so it has to be a customer ID with link
+            else:
+                #get customer link from table cell
+                customer_link = cells[0].find('a').get('href')
+                #ensure the link found is a customer link, sometimes org links and those arent helpful in this case
+                if "https://whois.arin.net/rest/customer/" in customer_link:
+                    #check if count is less than total
+                    if customer_link_check < total_customers:
+                        customer_link_check += 1
+                        progres(customer_link_check, total_customers, "Customers Check")
                         #append the IP address of the customer to the known IP addresses
-                        known_ip_addresses.append(addr)
-                else:
-                    #parse the customer data as an XML
-                    customer_soup = BeautifulSoup(requests.get(customer_link).text, 'xml')
-                    netref = customer_soup.find('netRef')
-                    addr = netref['startAddress'] + " - " + netref['endAddress']
-                    #append the IP address of the customer to the known IP addresses
-                    known_ip_addresses.append(addr)
+                        known_ip_addresses.append(customerScrape(customer_link))
 
+
+print("\n\n")
 print("-" * 64)
 print("\tArin IP address scraper by MrDaGree")
 print("\nSearch Queried: " + args.search)
-if args.limit:
-    print("Search Sub-Customer Limit: " + str(args.limit))
-else:
-    print("Search Sub-Customer Limit: ALL")
 print("\nKnown IP Addresses:")
 for ip in known_ip_addresses:
     print(ip)
